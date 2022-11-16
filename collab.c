0a153927b5a1620bff42a37fa6c5e35b27d77c6a
Daniel Luangnikone <57425315+nufrslulu@users.noreply.github.com>
2022-11-16 18:27:44 +0000

    moved collab.c outside of the folder to make it accessible

---
+++ collab.c
@@ -0,0 +1,39 @@
+// Collaborative code file, Lab 13
+// 
+// One person in your partner group should clone this repository.
+// Create a function that outputs your name, along with either one thing 
+//    that you learned about git or GitHub or something that gave you trouble
+//    that you would like others to learn from. The function name should be
+//    your NetID combined with your partner's NetID.
+// Feel free to add other things to your function as well, though be mindful
+// of the length of it, given how many students will be collaborating on this code!
+// 
+// Read through the code that is already here before adding your own.
+// Add this code to the repository using git commands.
+// 
+// An example is shown below.
+// 
+// UIC CS 211, Fall 2022
+#include <stdio.h>
+
+// void reed_ekidan2() {
+//   printf("Dale Reed and Ellen Kidane\n");
+//   printf("We learned that git and GitHub are amazing, powerful tools!\n");
+// }
+
+void dluan3_mkohli4(){
+  printf("Daniel Luangnikone\n");
+  printf("What I learned about github is that it makes it easy for developers to share code files and collaborate with fellow developers on open-source projects.\n");
+  printf("Manav Kohli \n");
+  printf("I learned that GitHub is a useful tool to help programmers collaborate and work on different aspects of the same project/product, allowing for a more seamless user experience.");
+}
+
+int main()
+{
+    printf("This code has been created by the students of CS 211, Fall 2022!\n\n");
+    
+    // reed_ekidan2();
+    dluan3_mkohli4();
+    
+    return 0;
+}
